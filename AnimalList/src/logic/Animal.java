77e8ef270b49e9d4c8306f47406ce8a923523849
b-shep <b.shep.git@gmail.com>
2019-05-14 16:04:26 -0400

    java project updates

---
+++ AnimalList/src/logic/Animal.java
@@ -0,0 +1,33 @@
+package logic;
+
+import java.util.function.Consumer;
+
+public abstract class Animal {
+	
+	private String name;
+	
+	
+	public Animal (String name) {
+		
+		 
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getNameAndType() {
+		return name + " the " + this.getClass();
+	}
+	
+	public abstract void speak();
+	
+	protected void speak (Consumer<Animal> consumer) {
+		
+	}
+	
+	
+}
